d05d415ae42329a7d99ab70477c36eaf4e7b46b2
guidoenr <guidoenr4@gmail.com>
2022-08-23 19:05:50 -0300

    guido:debian | 23-Aug-19:05

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
 func main() {
-	//models.InitDB()
-	//turnero.InitializeTurnos()
-	//turno := models.Turno{
-	//	BaseModel: bun.BaseModel{},
-	//	ID:        2,
-	//	OwnerId:   1,
-	//}
-	//controllers.ReserveTurno(&turno, 0)
-	// models.InitDB()
-	//InitializeTurnos()
 	ListenAndServe()
 }
--- router.go
+++ router.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/guidoenr/padel-field/controllers"
 	"net/http"
 )
 
+// ListenAndServe turns on the gin-gonic server and initialize the entire REST-API
+// routes and endpoints
 func ListenAndServe() {
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Turnos",
-		})
-		controllers.GetAvailableTurnos()
-	})
+	router.GET("/", index())
 
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	router.Run()
+}
+
+// index is the main page for the website
+func index() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Turnos",
+		})
+	}
 }
